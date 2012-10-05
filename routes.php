38936900b42567876a172916717a10f34abf351e
Wes Kempa <weskempa@gmail.com>
2012-10-04 22:14:31 -0400

    Added new routes

--- routes.php
+++ routes.php
@@ -7,10 +7,28 @@ $routes = array(
     array( '^$', 'pages.home' ),
     array( '^secure/$', 'pages.secure' ),
     array( '^login/$', 'account.login'),
+    
     array( '^glossary/$', 'glossary.index'),
     array( '^glossary/([-a-z]+)/$', 'glossary.show'),
     array( '^glossary/([-a-z]+)/delete/$', 'glossary.delete'),
     array( '^glossary/([-a-z]+)/edit/$', 'glossary.edit'),
+    array( '^glossary/([-a-z]+)/add/$', 'glossary.add'),
     array( '^glossary/([-a-z]+)/update/$', 'glossary.update'),
+
+    array( '^trainee/$', 'trainee.index'),
+    array( '^trainee/([-a-z]+)/$', 'trainee.show'),
+    array( '^trainee/([-a-z]+)/edit/$', 'trainee.edit'),
+    array( '^trainee/([-a-z]+)/add/$', 'trainee.add'),
+    array( '^trainee/([-a-z]+)/update/$', 'trainee.update'),
+
+    array( '^performance/([-a-z]+)/$', 'performance.show'),
+    array( '^performance/([-a-z]+)/edit/$', 'performance.edit'),
+    array( '^performance/([-a-z]+)/update/$', 'performance.update'),
+
+    array( '^module/([-a-z]+)/$', 'module.show'),
+    array( '^module/([-a-z]+)/edit/$', 'module.edit'),
+    array( '^module/([-a-z]+)/update/$', 'module.update'),
+
+    array( '^log/([-a-z]+)/$', 'log.show'),
 );
 ?>
