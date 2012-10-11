878479a429efa674f47bb840f49c610ba7b281ce
Wes Kempa <weskempa@gmail.com>
2012-10-10 23:03:16 -0400

    Added CSS,Images, JS for Form elements and Data grid sorting.

--- views/_header.php
+++ views/_header.php
@@ -4,7 +4,10 @@
         <title>Steelcase Administration</title>
         <meta charset="UTF-8" />
         <link rel="stylesheet" href="<?php static_url(); ?>css/style.css">
+        <link rel="stylesheet" href="<?php static_url(); ?>css/forms.css">
+        <link rel="stylesheet" href="<?php static_url(); ?>css/sorting_table.css">
         <script src="<?php static_url(); ?>js/jquery-1.8.2.min.js"></script>
+        <script src="<?php static_url(); ?>js/jquery.dataTables.min.js"></script>
     </head>
     <body>
         <div id="wrapper">
--- views/glossary/index.php
+++ views/glossary/index.php
@@ -1,26 +1,55 @@
 <?php echo render('_header') ?>
 <h1>Glossary Index</h1>
 <div class="add-new"><a href="<?php url('glossary.add', array("new")) ?>">Add new glossary item</a></div>
-<table>
-    <tr>
-        <th>Term</th>
-        <th>Definition</th>
-        <th>User</th>
-        <th>Created At</th>
-        <th>Updated At</th>
-        <th colspan="2">Actions</th>
-    </tr>
+
+<table cellpadding="0" cellspacing="0" border="0" class="display" id="assetlist">
+    <thead>
+        <tr>
+            <th>Term</th>
+            <th>Definition</th>
+            <th>User</th>
+            <th>Created At</th>
+            <th>Updated At</th>
+            <th>Actions</th>
+        </tr>
+    </thead>
+    <tbody>
     <?php foreach($terms as $term): ?>
-    <tr>
-        <td><?php echo $term->name ?></td>
-        <td><?php echo $term->description ?></td>
-        <td><?php echo $term->user_id ?></td>
-        <td><?php echo $term->created_at->format("F j, Y") ?></td>
-        <td><?php echo $term->updated_at->format("F j, Y") ?></td>
-        <td><a href="<?php url('glossary.edit', array($term->slug)) ?>">Edit</a></td>
-        <td><a href="<?php url('glossary.delete', array($term->slug)) ?>">&times;</a></td>
-    </tr>
+        <tr>
+            <td><?php echo $term->name ?></td>
+            <td><?php echo $term->description ?></td>
+            <td><?php echo $term->user_id ?></td>
+            <td><?php echo $term->created_at->format("F j, Y") ?></td>
+            <td><?php echo $term->updated_at->format("F j, Y") ?></td>
+            <td><a href="<?php url('glossary.edit', array($term->slug)) ?>">Edit</a> | <a href="<?php url('glossary.delete', array($term->slug)) ?>">&times;</a></td>
+        </tr>
     <?php endforeach; ?>
+    </tbody>
 
 </table>
+
+
+<script type="text/javascript" charset="utf-8">
+
+    /* Define two custom functions (asc and desc) for string sorting */
+    jQuery.fn.dataTableExt.oSort['string-case-asc']  = function(x,y) {
+        return ((x < y) ? -1 : ((x > y) ?  1 : 0));
+    };
+    
+    jQuery.fn.dataTableExt.oSort['string-case-desc'] = function(x,y) {
+        return ((x < y) ?  1 : ((x > y) ? -1 : 0));
+    };
+
+    /* Build the DataTable with third column using our custom sort functions */
+    jQuery('#assetlist').dataTable( {
+        "sPaginationType": "full_numbers",
+        "aaSorting": [ [0,'asc'] ],
+        "aoColumnDefs": [
+            { "sType": 'string-case', "aTargets": [ 2 ] }
+        ]
+    
+    } );    
+    
+</script>
+
 <?php echo render('_footer') ?>
