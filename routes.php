5ab87891483d0e3ddbae59350be8ffbf7d998aeb
Jay <jayzawrotny@gmail.com>
2012-10-13 22:01:00 -0400

    Fixed routes to support numbers in slugs.

--- routes.php
+++ routes.php
@@ -10,23 +10,23 @@ $routes = array(
     
     array( '^glossary/$', 'glossary.index'),
     array( '^glossary/add/$', 'glossary.add'),
-    array( '^glossary/([-a-z]+)/$', 'glossary.show'),
-    array( '^glossary/([-a-z]+)/delete/$', 'glossary.delete'),
-    array( '^glossary/([-a-z]+)/edit/$', 'glossary.edit'),
+    array( '^glossary/([-a-z0-9]+)/$', 'glossary.show'),
+    array( '^glossary/([-a-z0-9]+)/delete/$', 'glossary.delete'),
+    array( '^glossary/([-a-z0-9]+)/edit/$', 'glossary.edit'),
 
     array( '^trainee/$', 'trainee.index'),
-    array( '^trainee/([-a-z]+)/$', 'trainee.show'),
-    array( '^trainee/([-a-z]+)/edit/$', 'trainee.edit'),
+    array( '^trainee/([-a-z0-9]+)/$', 'trainee.show'),
+    array( '^trainee/([-a-z0-9]+)/edit/$', 'trainee.edit'),
     array( '^trainee/add/$', 'trainee.add'),
-    array( '^trainee/([-a-z]+)/update/$', 'trainee.update'),
+    array( '^trainee/([-a-z0-9]+)/update/$', 'trainee.update'),
 
-    array( '^performance/([-a-z]+)/$', 'performance.show'),
-    array( '^performance/([-a-z]+)/edit/$', 'performance.edit'),
-    array( '^performance/([-a-z]+)/update/$', 'performance.update'),
+    array( '^performance/([-a-z0-9]+)/$', 'performance.show'),
+    array( '^performance/([-a-z0-9]+)/edit/$', 'performance.edit'),
+    array( '^performance/([-a-z0-9]+)/update/$', 'performance.update'),
 
-    array( '^module/([-a-z]+)/$', 'module.show'),
-    array( '^module/([-a-z]+)/edit/$', 'module.edit'),
-    array( '^module/([-a-z]+)/update/$', 'module.update'),
+    array( '^module/([-a-z0-9]+)/$', 'module.show'),
+    array( '^module/([-a-z0-9]+)/edit/$', 'module.edit'),
+    array( '^module/([-a-z0-9]+)/update/$', 'module.update'),
 
     array( '^log/([-a-z]+)/$', 'log.show'),
 
