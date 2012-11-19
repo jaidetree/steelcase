9557697aa41abcd6e6f3e6fd27fbfca86de02830
Wes Kempa <weskempa@gmail.com>
2012-11-18 19:33:46 -0500

    Fixed CSS, Dashboard Dummy Improvments

--- views/_header.php
+++ views/_header.php
@@ -3,18 +3,18 @@
     <head>
         <title>Steelcase Administration</title>
         <meta charset="UTF-8" />
-        <link rel="stylesheet" href="<?php static_url(); ?>css/reset.css">
-        <link rel="stylesheet" href="<?php static_url(); ?>css/sorting_table.css">
-        <link rel="stylesheet" href="<?php static_url(); ?>css/forms.css">
-        <link rel="stylesheet" href="<?php static_url(); ?>css/style.css">
+        <link rel="stylesheet" href="<?php static_url(); ?>css/reset.css" type="text/css">
+        <link rel="stylesheet" href="<?php static_url(); ?>css/sorting_table.css" type="text/css">
+        <link rel="stylesheet" href="<?php static_url(); ?>css/forms.css" type="text/css">
+        <link rel="stylesheet" href="<?php static_url(); ?>css/style.css" type="text/css">
+        <link rel="stylesheet" href="<?php static_url(); ?>css/dash.css" type="text/css"/>
         <script src="<?php static_url(); ?>js/jquery-1.8.2.min.js"></script>
         <script src="<?php static_url(); ?>js/jquery.dataTables.min.js"></script>
         <script src="<?php static_url(); ?>js/ZeroClipboard.min.js"></script>
         <script src="<?php static_url(); ?>js/modal.js"></script>
         <script src="<?php static_url(); ?>js/crud.js"></script>
-        <script src="http://code.highcharts.com/highcharts.js"></script>
-		<script src="http://code.highcharts.com/modules/exporting.js"></script>
-        <script src="<?php static_url(); ?>js/test.js"></script>
+        <script src="<?php static_url(); ?>js/highcharts.js"></script>
+        <script src="<?php static_url(); ?>js/dashboard.js"></script>
     </head>
     <body>
         <div id="wrapper">
